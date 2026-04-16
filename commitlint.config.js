03df85cdf266e1351f3516e7d261e2fd6ea44cdb
dhwani-ankit <dhwani-ankit@users.noreply.github.com>
2026-04-16 12:09:21 +0530

    Revert "Reapply "chore: sync optimized workflows from frappe-repo-starter""
    This reverts commit fb58c8998e20337ae2f91692d7d33964a6a2757e.

--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  // Disable default 100-char (or 72-char) header length limit for commit messages
-  rules: {
-    "header-max-length": [0, "always", 100],
-  },
-};
